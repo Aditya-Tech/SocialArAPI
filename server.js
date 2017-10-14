066bc33d99052bbe38a71a5443b92052be63a723
Aditya Paruchuri <adityaparuchuri@eduroam-128-61-99-139.eduroam.gatech.edu>
2017-10-14 00:28:37 -0400

    get request with dummy data for testing

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ function handleError(res, reason, message, code) {
 
 
 app.get("/", function(req, res) {
+
   db.collection("ar").find({}).toArray(function(err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get data.");
@@ -46,10 +47,26 @@ app.get("/", function(req, res) {
   });
 })
 
-app.post("/addNewLocation/:lat/:lon", function(req, res) {
 
-  var lat = req.params.lat
-  var lon = req.params.lon
+app.get("/:lat/:lon", function(req, res) {
+  //console.log(db.collection("ar").find())
+    if (db.collection("ar").find({"latitude": req.params.lat, 'longitude': req.params.lon}).count() == 0) {
+      console.log("LOCATION DOESN'T EXIST IN DATABASE.")
+      res.status(400).send("LOCATION DOESN'T EXIST IN DATABASE.")
+    } else {
+      // console.log(db.collection("ar").find({"latitude": req.params.lat, 'longitude': req.params.lon}).toArray());
+      // res.status(200).send(db.collection("ar").find({"latitude": req.params.lat, 'longitude': req.params.lon}).toArray());
+      // console.log(JSON.stringify(db.collection("ar").find()));
+      // res.status(200).send(db.collection("ar").find());
+    }
+    console.log("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam")
+    res.status(400).send("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam")
+
+})
+
+app.post("/addNewLocation/:lat/:lon", function(req, res) {
+  var lat = Math.round(req.params.lat / 10000) * 10000
+  var lon = Math.round(req.params.lon / 10000) * 10000
   console.log("Latitude: " + lat)
   console.log("Longitude: " + lon)
 
@@ -60,9 +77,14 @@ app.post("/addNewLocation/:lat/:lon", function(req, res) {
   var body = {
     "latitude" : lat,
     "longitude" : lon,
-    "username" : req.body.username,
-    "text" : req.body.textpost
+    "posts" : [
+      {
+        "username" : req.body.username,
+        "text" : req.body.textpost
+      }
+    ]
   }
+    
 
   db.collection("ar").insertOne(body, function(err) {
     if (err) {
@@ -72,3 +94,11 @@ app.post("/addNewLocation/:lat/:lon", function(req, res) {
     console.log(body);
   });       
 });
+
+
+app.get("/getNearest/:lat/:lon", function(req, res) {
+
+
+
+
+})
