95ba3ff7b48c8707f3de4e46bba2a99da31606ab
Aditya Paruchuri <adityaparuchuri@eduroam-128-61-99-139.eduroam.gatech.edu>
2017-10-14 01:41:18 -0400

    get request with dummy data for testing

--- server.js
+++ server.js
@@ -49,51 +49,49 @@ app.get("/", function(req, res) {
 
 
 app.get("/:lat/:lon", function(req, res) {
-  //console.log(db.collection("ar").find())
-    if (db.collection("ar").find({"latitude": req.params.lat, 'longitude': req.params.lon}).count() == 0) {
-      console.log("LOCATION DOESN'T EXIST IN DATABASE.")
-      res.status(400).send("LOCATION DOESN'T EXIST IN DATABASE.")
-    } else {
-      // console.log(db.collection("ar").find({"latitude": req.params.lat, 'longitude': req.params.lon}).toArray());
-      // res.status(200).send(db.collection("ar").find({"latitude": req.params.lat, 'longitude': req.params.lon}).toArray());
-      // console.log(JSON.stringify(db.collection("ar").find()));
-      // res.status(200).send(db.collection("ar").find());
-    }
+    // if (db.collection("ar").find({"latitude": Math.round(req.params.lat * 10000) / 10000 , 'longitude': Math.round(req.params.lon * 10000) / 10000}).count() == 0) {
+    //   console.log("LOCATION DOESN'T EXIST IN DATABASE.")
+    //   res.status(400).send("LOCATION DOESN'T EXIST IN DATABASE.")
+    // } else {
+    //   console.log(db.collection("ar").find({"latitude": Math.round(req.params.lat * 10000) / 10000, 'longitude': Math.round(req.params.lon * 10000) / 10000}));
+    //   res.status(200).send(db.collection("ar").find({"latitude": req.params.lat, 'longitude': req.params.lon}));
+    //   // console.log(JSON.stringify(db.collection("ar").find()));
+    //   // res.status(200).send(db.collection("ar").find());
+    // }
+
     console.log("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam")
     res.status(400).send("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam")
 
 })
 
 app.post("/addNewLocation/:lat/:lon", function(req, res) {
-  var lat = Math.round(req.params.lat / 10000) * 10000
-  var lon = Math.round(req.params.lon / 10000) * 10000
+  var lat = Math.round(req.params.lat * 10000) / 10000 
+  var lon = Math.round(req.params.lon * 10000) / 10000
+  // var lat = req.params.lat.toFixed(5)
+  // var lon = req.params.lon.toFixed(5)
   console.log("Latitude: " + lat)
   console.log("Longitude: " + lon)
 
-  if((!req.body.username || typeof req.body.username != "string") || (!req.body.textpost || typeof req.body.textpost != "string")) {
-    res.status(400).send("400 Bad Request")
-  }
-
-  var body = {
-    "latitude" : lat,
-    "longitude" : lon,
-    "posts" : [
-      {
-        "username" : req.body.username,
-        "text" : req.body.textpost
-      }
-    ]
-  }
-    
-
-  db.collection("ar").insertOne(body, function(err) {
-    if (err) {
-      console.log("Error adding new post! " + err);
-      process.exit(1);
+  if (db.collection("ar").find({"latitude": req.params.lat, 'longitude': req.params.lon}).count() > 0) {
+    console.log("Entry already exists! Updating data...")
+  } else {
+    if((!req.body.username || typeof req.body.username != "string") || (!req.body.textpost || typeof req.body.textpost != "string")) {
+      res.status(400).send("400 Bad Request")
     }
-    console.log(body);
-  });       
-});
+
+    req.body["latitude"] = lat;
+    req.body["longitude"] = lon;
+  
+    db.collection("ar").insertOne(req.body, function(err) {
+      if (err) {
+        console.log("Error adding new post! " + err);
+        process.exit(1)
+      }
+      console.log(req.body)
+      res.status(400).send("New entry added!")
+    })
+  }        
+})
 
 
 app.get("/getNearest/:lat/:lon", function(req, res) {
